2f9a33e94c5e23ea7c7874a5d67b977c2bf59623
Leah LaSalla <leah@opensorceress.net>
2014-12-06 07:17:47 +0000

    Committing progress

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,6 +1,15 @@
-require '/home/jacqueline/ruby_executables/adrienne_xml/backup.yml'
+require 'yaml'
+#require 'nokogiri'
 
+#doc = (File.exists?("#{APP_ROOT}/backup.yml") ? YAML.load_file("#{APP_ROOT}/backup.yml") : nil)
 
+doc = YAML.load_file("#{APP_ROOT}/backup.yml")
 
-
-cnf = YAML::load_file(File.join(__dir__, 'config.yml'))
+output = Nokogiri::XML::Builder.new do |xml|
+    xml.object{
+        #xml.id doc["getOrderDetails"]["Id"]
+        #xml.name doc["getOrderDetails"]["Name"]
+        xml.id doc["attributes"]["id"]
+    }
+end
+puts output.to_xml
